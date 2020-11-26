eca6aed3cc93a9a2fc1fd1dbd334273fe74a15b0
maersksan <maersksan@gmail.com>
2020-11-26 18:21:52 +0900

    課題５、６の内容を、日付時刻をファイル名としたレシートファイル（テキスト）に出力

--- pos-system.py
+++ pos-system.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+import datetime
 
 
 ### 商品クラス
@@ -34,14 +35,21 @@ class Order:
 
     def show_all(self):
         total_amount = 0
+        purchase_list = []
         for item_master in self.item_master:
             for item_code, quantity in self.item_order_list.items():
                 if item_code in item_master.item_code:
                     total_amount += (item_master.price * quantity)
+                    purchase_list.append([item_master.item_name, quantity, item_master.price])
                     print('商品名:{} 商品価格:{}'.format(item_master.item_name, item_master.price))
                     print('個数{}'.format(quantity))
         print('合計金額: {}'.format(total_amount))
-        return total_amount
+        return total_amount, purchase_list
+
+    def save_purchase_list(self, item_name, item_price, quantity):
+        with open('hoge.txt', 'a') as f:
+            print()
+            print('商品名: {} 商品価格: {}　購入個数: {}'.format(item_name, item_price, quantity), file=f)
 
 
 # 商品オーダー時にマスターに登録されている商品か調べる関数
@@ -51,6 +59,17 @@ def isvalid_order_code(master, order_item_code):
             return order_item_code
     print('入力内容が間違っています', file=sys.stderr)
     sys.exit()
+
+# txtファイルにして保存
+def save_file(purchase_list, total_amount, customer_payment, return_change):
+    now = datetime.datetime.now()
+    filename = './output/log_' + now.strftime('%Y%m%d_%H%M%S') + '.txt'
+    with open(filename, 'a') as f:
+        for row in purchase_list:
+            f.write('商品名: {} 商品価格: {}　購入個数: {}\n'.format(row[0], row[2], row[1]))
+        f.write('合計金額: {}円\n'.format(str(total_amount)))
+        f.write('お預かり: {}円\n'.format(customer_payment))
+        f.write('お釣り: {}円\n'.format(return_change))
 
 
 ### メイン処理
@@ -85,8 +104,8 @@ def main():
         # オーダー表示
         order.view_item_list()
         # オーダー登録した商品の一覧（商品名、価格）を表示し、かつ合計金額、個数を表示
-        total_amount += order.show_all()
-
+        total_amount, purchase_list = order.show_all()
+        print(total_amount, purchase_list)
     # お釣り計算
     customer_payment = input('お金をお支払いください>> ')
     if not customer_payment:
@@ -96,27 +115,12 @@ def main():
         print('金額が足りていません')
         return
     return_change = int(customer_payment) - total_amount
+    # txtファイルを保存
     if return_change > 0:
-        print('お釣りは{}円です'.format(return_change))
-        print('ありがとうございました')
+        save_file(purchase_list, total_amount, customer_payment, return_change)
     else:
-        print('ありがとうございました')
-
-
-# order = Order(item_master)
-# order.add_item_order("001")
-# order.add_item_order("002")
-# order.add_item_order("003")
-# order.view_item_detail()
-# # オーダー表示
-# order.view_item_list()
+        save_file(purchase_list, total_amount, customer_payment, return_change=0)
 
 
 if __name__ == "__main__":
     main()
-# Column = ['商品コード', '商品名', '価格']
-# lista = ["001", "りんご", 100]
-# listb = ["002", "なし", 120]
-# listc = ["003", "みかん", 150]
-# df = pd.DataFrame([lista, listb, listc], columns=Column)
-# df.to_csv("products_master.csv", index=False)
